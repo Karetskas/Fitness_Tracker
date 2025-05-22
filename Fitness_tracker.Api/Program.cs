efb86dc19523a986c6e827c07ef77859e6da34fc
Karetskas_Evgeny <kub99999@yandex.ru>
2025-05-22 22:35:10 +0500

    Создана сущность User. Настроено подключение к БД PostgreSQL. Создана миграция с таблицей User.

--- Fitness_tracker.Api/Program.cs
+++ Fitness_tracker.Api/Program.cs
@@ -1,3 +1,6 @@
+using Fitness_tracker.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 namespace Fitness_tracker.Api
 {
     public class Program
@@ -5,6 +8,9 @@ namespace Fitness_tracker.Api
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<UserContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
+
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
---
+++ Fitness_tracker.Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using Fitness_tracker.Domain.Interfaces;
+
+namespace Fitness_tracker.Domain.Entities
+{
+    public sealed class User : IEntity
+    {
+        public Guid Id { get; set; }
+        
+        public DateTime Created { get; set; }
+
+        public string Username { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string? AvatarUrl { get; set; }
+
+        public DateTime? Deleted { get; set; }
+    }
+}
